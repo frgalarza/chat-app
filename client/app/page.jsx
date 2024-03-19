e249572fcdb76036923d0c6fc6565b86e3d3d3e9
Franco Galarza <francig4444@gmail.com>
2024-03-19 20:02:35 -0300

    page to jsx

---
+++ client/app/page.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <aside>
+        <ul>
+          <li>User 1</li>
+          <li>User 2</li>
+          <li>User 3</li>
+        </ul>
+      </aside>
+      <section>
+      </section>
+    </main>
+  );
+}
