b715eeb94371cb0b144b0ff39b25f852f697b02b
Franco Galarza <francig4444@gmail.com>
2024-03-19 14:50:48 -0300

    server ready

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const connection = require('./db_conn')
+const { UserModel } = require('./Models/UserModel')
+const { Server } = require('socket.io')
+const { createServer } = require('http')
+const httpServer = createServer(app)
+const io = new Server(httpServer)
 
 app.use(cors())
 
@@ -15,4 +20,32 @@ app.use((req, res, next)=>{
 
 app.use(express.json())
 
-module.exports = app
+const obj = {}
+io.on('connection', (socket)=>{
+
+    socket.on('createConnection', (userId)=>{
+        obj[userId] = socket.id
+    })
+
+    socket.on('chatMsg', async (msg, receiverId, senderId) => {
+        let newMsg = {msg, receiverId, senderId}
+
+        await UserModel.updateOne(
+            { _id: senderId }, 
+            { $push: {chats: newMsg} }
+        )
+
+        await UserModel.updateOne(
+            { _id: receiverId },
+            { $push: { chats: newMsg } }
+        )
+
+        io.to(obj[receiverId].emit('received msg', msg, senderId))
+
+        socket.on('disconnect', ()=>{
+            console.log('User disconnected');
+        })
+    })
+})
+
+module.exports = httpServer
--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const server = require('./app')
 const PORT = 3001
+const connection = require('./db_conn')
 
-server.listen(PORT, ()=>{console.log('Server esscuchando en el puerto: ' + PORT);})
+server.listen(PORT, async()=>{
+    try {
+        await connection
+        console.log('Connected to DB');
+        console.log('Server listen on port: ' + PORT);
+    } catch (error) {
+        console.log(error);
+    }
+})
